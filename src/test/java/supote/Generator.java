16d0666a9e6ba42c5dd4b8eac8692978e0bcc804
Marco Auelio <testingmarco@outlook.com>
2022-04-19 18:14:22 -0300

    Initial commit

---
+++ src/test/java/supote/Generator.java
@@ -0,0 +1,11 @@
+package supote;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
+public class Generator {
+    public static String dataHoraParaArquivo() {
+        Timestamp ts = new Timestamp(System.currentTimeMillis());
+        return new SimpleDateFormat("yyyMMddhhmmss").format(ts);
+    }
+}
